2b2f83487b2ca2acacdcea507e31078921c6d8aa
RachaelMiki1215 <rachaelbuxton1215@gmail.com>
2023-07-07 02:02:58 -0500

    Created carousel component for About section

---
+++ src/components/container/Carousel.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import Styles from "./Carousel.module.scss";
+
+import { useEffect, useState } from "react";
+import React from "react";
+
+export default function Carousel({
+  containerClassName,
+  pageContainerClassName,
+  navigatorClassName,
+  children,
+}: {
+  containerClassName?: string;
+  pageContainerClassName?: string;
+  navigatorClassName?: string;
+  children: React.ReactNode;
+}) {
+  const [currSlide, setCurrSlide] = useState<number>(0);
+  const [slideCount, setSlideCount] = useState<number>(0);
+
+  useEffect(() => {
+    setSlideCount(React.Children.count(children));
+  }, [children]);
+
+  return (
+    <div className={`${Styles.container} ${containerClassName}`}>
+      <div className={`${Styles.pageContainer} ${pageContainerClassName}`}>
+        {React.Children.map(children, (child, index) => {
+          return (
+            <div
+              className={Styles.page}
+              style={{ transform: `translateX(${(index - currSlide) * 100}%)` }}
+            >
+              {child}
+            </div>
+          );
+        })}
+      </div>
+      <div className={`${Styles.navigatorContainer} ${navigatorClassName}`}>
+        {React.Children.map(children, (child, index) => {
+          return (
+            <button
+              type="button"
+              className={`${Styles.navBtn} ${
+                currSlide === index ? Styles.active : ""
+              }`}
+              onClick={() => {
+                setCurrSlide(index);
+              }}
+            ></button>
+          );
+        })}
+      </div>
+      <button
+        type="button"
+        className={Styles.prevBtn}
+        onClick={() => {
+          setCurrSlide(() => {
+            return currSlide - 1;
+          });
+        }}
+        disabled={currSlide < 1}
+      >
+        &lt;
+      </button>
+      <button
+        type="button"
+        className={Styles.nextBtn}
+        onClick={() => {
+          setCurrSlide(() => {
+            return currSlide + 1;
+          });
+        }}
+        disabled={currSlide >= slideCount - 1}
+      >
+        &gt;
+      </button>
+    </div>
+  );
+}
