11e2e9452f4840f54f77c574336dc6ccc880d07d
RachaelMiki1215 <rachaelbuxton1215@gmail.com>
2023-07-07 02:07:28 -0500

    Added metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
-"use client";
-
-import { majorMonoDisplay } from "../components/next-fonts";
+import { Metadata } from "next";
+import { majorMonoDisplayClass } from "../components/next-fonts";
 
 import Styles from "./home.module.scss";
 
+//TODO: Figure out website keywords later.
+export const metadata : Metadata = {
+  title: "Rachael Miki Buxton | Portfolio Website",
+  description: "This website showcases projects done by Rachael Miki Buxton.",
+};
+
 export default function Home() {
   return (
-    <main className={`${majorMonoDisplay.className} ${Styles.main}`}>
+    <main className={`${majorMonoDisplayClass} ${Styles.main}`}>
       Hey There,
       <br />Nice To
       <br />See You
