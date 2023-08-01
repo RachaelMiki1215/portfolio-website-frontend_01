5e6e1be5c1aa891f94fb79c970c596cce1657ee8
RachaelMiki1215 <rachaelbuxton1215@gmail.com>
2023-08-01 13:32:46 -0500

    Changed function component to arrow function component

--- src/components/strings/greetings.tsx
+++ src/components/strings/greetings.tsx
@@ -1,23 +1,25 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from "react";
 
-export default function TimeBasedGreeting() {
-    const [greeting, setGreeting] = useState("Hello");
-    
-    useEffect(() => {
-        let now = new Date();
-    
-        if (now.getHours() >= 4 && now.getHours() < 12) {
-          setGreeting("Good morning");
-        } else if (now.getHours() >= 12 && now.getHours() < 17) {
-          setGreeting("Good afternoon");
-        } else if (now.getHours() >= 17 && now.getHours() < 20) {
-          setGreeting("Good evening");
-        } else {
-          setGreeting("Hey there");
-        }
-      }, []);
+const TimeBasedGreeting: React.FC = () => {
+  const [greeting, setGreeting] = useState("Hello");
 
-    return <>{greeting}</>;
-}
+  useEffect(() => {
+    const now: Date = new Date();
+
+    if (now.getHours() >= 4 && now.getHours() < 12) {
+      setGreeting("Good morning");
+    } else if (now.getHours() >= 12 && now.getHours() < 17) {
+      setGreeting("Good afternoon");
+    } else if (now.getHours() >= 17 && now.getHours() < 20) {
+      setGreeting("Good evening");
+    } else {
+      setGreeting("Hey there");
+    }
+  }, []);
+
+  return <>{greeting}</>;
+};
+
+export default TimeBasedGreeting;
