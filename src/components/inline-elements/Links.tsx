fb622176cc1780ad55afd0a348029f18e7f78c8e
RachaelMiki1215 <rachaelbuxton1215@gmail.com>
2023-08-01 13:44:17 -0500

    Isolated InlineLink component props to a type declaration

--- src/components/inline-elements/Links.tsx
+++ src/components/inline-elements/Links.tsx
@@ -6,19 +6,18 @@ import Styles from "./Links.module.scss";
 import Link from "next/link";
 import { faLink } from "@fortawesome/free-solid-svg-icons";
 
-export const InlineLink = ({
-  href,
-  children,
-}: {
+type InlineLinkProps = {
   href: string;
   children: React.ReactNode;
-}) => {
+};
+
+export const InlineLink: React.FC<InlineLinkProps> = ({ href, children }) => {
   return (
     <Link href={href} className={Styles.inlineContainer}>
       <span className={Styles.text}>{children}</span>
-      <div className={Styles.overlay}>
+      <span className={Styles.overlay}>
         <FontAwesomeIcon icon={faLink} />
-      </div>
+      </span>
     </Link>
   );
 };
