dfd5995acad1139c544c1e9eb97c573f2bfffb34
RachaelMiki1215 <rachaelbuxton1215@gmail.com>
2023-08-01 13:46:18 -0500

    Added Resume page

--- src/app/about/resume/page.tsx
+++ src/app/about/resume/page.tsx
@@ -1,5 +1,57 @@
 // TODO: Complete this page
 
-export default function Resume() {
-  return <></>;
+import Container from "@/components/container/Container";
+import Styles from "./Resume.module.scss";
+import ResumeGrid from "./resumeGrid";
+import { Metadata } from "next";
+import {
+  getCertifications,
+  getEducation,
+  getHobbies,
+  getSkills,
+} from "@/data/FetchFromMyDb";
+
+export const metadata: Metadata = {
+  title: "Rachael Miki Buxton | Portfolio Website | Resume",
+  description: "Author's resume.",
+};
+
+export default async function Resume() {
+  const education = await getEducation();
+  const certifications = await getCertifications();
+  const hobbies = await getHobbies();
+  const skills = await getSkills();
+
+  return (
+    <ResumeGrid>
+      <Container className={Styles.nameSection}></Container>
+      <Container className={Styles.educationSection}>
+        <h1>Education</h1>
+        {/* {education.map((edu: any) => {
+          return <div>{edu}</div>;
+        })} */}
+      </Container>
+      <Container className={Styles.certificationSection}>
+        <h1>Certifications</h1>
+      </Container>
+      <Container className={Styles.hobbySection}>
+        <h1>Hobbies</h1>
+        {/* {hobbies.map((hobby: any) => {
+          return <div>{hobby}</div>;
+        })} */}
+      </Container>
+      <Container className={Styles.projectSection}>
+        <h1>Projects</h1>
+      </Container>
+      <Container className={Styles.workExperienceSection}>
+        <h1>Work Experience</h1>
+      </Container>
+      <Container className={Styles.skillSection}>
+        <h1>Skills</h1>
+        {/* {skills.map((skill: any) => {
+          return <div>{skill.id}</div>;
+        })} */}
+      </Container>
+    </ResumeGrid>
+  );
 }
---
+++ src/app/about/resume/resumeGrid.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { ReactNode, useContext } from "react";
+import Styles from "./Resume.module.scss";
+import ContentContext from "@/context/ContentContext";
+
+const ResumeGrid: React.FC<{ children?: React.ReactNode }> = ({
+  children,
+}: {
+  children?: ReactNode;
+}) => {
+  const contentArea = useContext(ContentContext);
+
+  return (
+    <main
+      className={Styles.main}
+      style={{ minHeight: `${Math.max(contentArea.height - 10, 500)}px` }}
+    >
+      {children}
+    </main>
+  );
+};
+
+export default ResumeGrid;
