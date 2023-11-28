37b1c60f14def82f3212324cdec7125cc1256402
Rachael Miki Buxton <rachaelbuxton1215@gmail.com>
2023-11-27 23:22:13 -0600

    Initiated new Modal component (dev in progress)

--- src/components/container/ContainerTypes.d.ts
+++ src/components/container/ContainerTypes.d.ts
@@ -1,21 +1,28 @@
 export interface CarouselProps {
-    containerClassName?: string;
-    pageContainerClassName?: string;
-    navigatorClassName?: string;
-    children: React.ReactNode;
+  containerClassName?: string;
+  pageContainerClassName?: string;
+  navigatorClassName?: string;
+  children: React.ReactNode;
 }
 
 export interface SlidingDropDownProps {
-    headerComponent: React.ReactNode;
-    dropdownComponent: React.ReactNode;
-    dropdownStyle?: React.CSSProperties;
-    isOpenOnDefault?: boolean;
-    changeOnClick?: boolean;
-    openOnHover?: boolean;
+  headerComponent: React.ReactNode;
+  dropdownComponent: React.ReactNode;
+  dropdownStyle?: React.CSSProperties;
+  isOpenOnDefault?: boolean;
+  changeOnClick?: boolean;
+  openOnHover?: boolean;
 }
 
 export interface ContainerProps {
-    children?: React.ReactNode;
-    className?: string;
-    style?: any;
+  children?: React.ReactNode;
+  className?: string;
+  style?: any;
+}
+
+// TODO: Finish up modal later on
+export interface ModalProps {
+  className?: string;
+  style?: any;
+  onBackgroundClick?: () => {};
 }
---
+++ src/components/container/Modal.tsx
@@ -0,0 +1,11 @@
+import { ModalProps } from "./ContainerTypes";
+
+// TODO: Finish up Modal Element
+
+const Modal: React.FC<ModalProps> = ({
+  className,
+  style,
+  onBackgroundClick,
+}) => {
+  return <></>;
+};
