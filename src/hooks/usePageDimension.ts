9bcc3d914fc08eeff78d1d04472788342ea338e4
Rachael Miki Buxton <rachaelbuxton1215@gmail.com>
2023-11-27 23:21:34 -0600

    Created custom hook for window size.

---
+++ src/hooks/usePageDimension.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+const useWindowSize = (): { width: number; height: number } => {
+  const [windowSize, setWindowSize] = useState<{
+    width: number;
+    height: number;
+  }>({
+    width: 0,
+    height: 0,
+  });
+
+  const handleResize = () => {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    // Call handler right away to update state with initial window size
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowSize;
+};
+
+export { useWindowSize as usewindowSize };
