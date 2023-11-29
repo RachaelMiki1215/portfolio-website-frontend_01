ac8f28b349bc66ee592b9cb604a9c53b570125f1
Rachael Miki Buxton <rachaelbuxton1215@gmail.com>
2023-11-28 22:35:39 -0600

    Added project list hover/highlighting

--- src/app/portfolio/layout.tsx
+++ src/app/portfolio/layout.tsx
@@ -1,68 +1,16 @@
-import Styles from "./portfolio.module.scss";
-
 import { getProjectsDynamic } from "@/data/FetchFromMyDb";
-import { formatDate } from "@/functions/DateFunctions";
 import { ProjectType } from "@/types/MyDbTypes";
 import React from "react";
 import Layout_LeftContentDisplay from "./layout_client";
-import Link from "next/link";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSuitcase } from "@fortawesome/free-solid-svg-icons";
 
-const ProjectList: React.FC = async () => {
-  // TODO: I know you hate to do so, but you probably should think about pagination soon.
-  const projects: ProjectType[] = await getProjectsDynamic();
-
-  return (
-    <ul className={Styles.projectList}>
-      {projects
-        .sort(
-          (a, b) => Date.parse(b.publishedDate) - Date.parse(a.publishedDate)
-        )
-        .map((project) => {
-          return (
-            <li className={Styles.projectItem} key={`project_${project.id}`}>
-              <span>
-                <span className={Styles.projectTitle}>{project.title}</span>
-                {project.didAtWorkId && <FontAwesomeIcon icon={faSuitcase} />}
-              </span>
-              <span className={Styles.projectDate}>
-                {formatDate(project.publishedDate)}
-              </span>
-              {project.keywords.length > 0 && (
-                <ul className={Styles.projectKeywords}>
-                  {project.keywords.map((keyword) => {
-                    return (
-                      <li
-                        key={`project_${project.id}_${keyword}`}
-                        className={Styles.projectKeyword}
-                      >
-                        {keyword}
-                      </li>
-                    );
-                  })}
-                </ul>
-              )}
-              <Link
-                href={`/portfolio/${project.id}`}
-                className={Styles.projectButton}
-              />
-            </li>
-          );
-        })}
-    </ul>
-  );
-};
-
-export default function PortfolioLayout({
+export default async function PortfolioLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const projects: ProjectType[] = await getProjectsDynamic();
+
   return (
-    <Layout_LeftContentDisplay
-      leftContent={<ProjectList />}
-      rightContent={children}
-    />
+    <Layout_LeftContentDisplay projects={projects} rightContent={children} />
   );
 }
--- src/app/portfolio/layout_client.tsx
+++ src/app/portfolio/layout_client.tsx
@@ -4,22 +4,86 @@ import Style from "./portfolio.module.scss";
 
 import { usePathname } from "next/navigation";
 
-import { usewindowSize } from "@/hooks/usePageDimension";
+import { useWindowSize } from "@/hooks/usePageDimension";
+import { ProjectType } from "@/types/MyDbTypes";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSuitcase } from "@fortawesome/free-solid-svg-icons";
+import { formatDate } from "@/functions/DateFunctions";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const ProjectList: React.FC<{ projects: ProjectType[]; currPath: string }> = ({
+  projects,
+  currPath,
+}) => {
+  // TODO: I know you hate to do so, but you probably should think about pagination soon.
+
+  const [currProjectId, setCurrProjectId] = useState<string>("");
+
+  useEffect(() => {
+    setCurrProjectId(currPath.split("/").pop());
+  }, [currPath]);
+
+  return (
+    <ul className={Style.projectList}>
+      {projects
+        .sort(
+          (a, b) => Date.parse(b.publishedDate) - Date.parse(a.publishedDate)
+        )
+        .map((project) => {
+          return (
+            <li
+              className={`${Style.projectItem} ${
+                currProjectId === project.id && Style.isSelected
+              }`}
+              key={`project_${project.id}`}
+            >
+              <span>
+                <span className={Style.projectTitle}>{project.title}</span>
+                {project.didAtWorkId && <FontAwesomeIcon icon={faSuitcase} />}
+              </span>
+              <span className={Style.projectDate}>
+                {formatDate(project.publishedDate)}
+              </span>
+              {project.keywords.length > 0 && (
+                <ul className={Style.projectKeywords}>
+                  {project.keywords.map((keyword) => {
+                    return (
+                      <li
+                        key={`project_${project.id}_${keyword}`}
+                        className={Style.projectKeyword}
+                      >
+                        {keyword}
+                      </li>
+                    );
+                  })}
+                </ul>
+              )}
+              <Link
+                href={`/portfolio/${project.id}`}
+                className={Style.projectButton}
+              />
+            </li>
+          );
+        })}
+    </ul>
+  );
+};
 
 type Layout_LeftContentDisplayType = {
-  leftContent: React.ReactNode | string;
+  projects: ProjectType[];
   rightContent: React.ReactNode | string;
 };
 
 const Layout_Client: React.FC<Layout_LeftContentDisplayType> = ({
-  leftContent,
+  projects,
   rightContent,
 }) => {
   const pathname = usePathname();
   const onPortfolioTop: boolean =
     pathname === "/portfolio" || pathname === "/portfolio/";
 
-  const windowSize = usewindowSize();
+  const windowSize = useWindowSize();
 
   return (
     <div
@@ -30,7 +94,9 @@ const Layout_Client: React.FC<Layout_LeftContentDisplayType> = ({
         }`,
       }}
     >
-      {(windowSize.width > 900 || onPortfolioTop) && leftContent}
+      {(windowSize.width > 900 || onPortfolioTop) && (
+        <ProjectList projects={projects} currPath={pathname} />
+      )}
       {rightContent}
     </div>
   );
